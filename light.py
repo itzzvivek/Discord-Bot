fd5eac4c6971e60026c7d36ee5287db34eae528e
Vivek Warkade <vivekwarkade000@gmail.com>
2023-07-23 23:45:27 +0530

    Update light.py

--- light.py
+++ light.py
@@ -2,7 +2,7 @@ import hikari
 import lightbulb
 
 bot = lightbulb.BotApp(
-    token='MTEzMTg1NTc5ODU4MzE4NTQ5MA.Gres6b.H0XRvIDxhdXIiQ6jkMSuRo8d9c9C4BLpSEcZ7c', 
+    token='***********', 
     default_enabled_guilds=(1131855540633489528))
 
 

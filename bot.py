427197fe66942af4e69691f1a6bb7c86f94254e0
Vivek Warkade <vivekwarkade000@gmail.com>
2023-07-23 16:13:36 +0530

    create events

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
 import hikari
 
-bot = hikari.GatewayBot(token='token no.')
+bot = hikari.GatewayBot(token='*********')
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.content)
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print("Bot has started")
+
 bot.run()
